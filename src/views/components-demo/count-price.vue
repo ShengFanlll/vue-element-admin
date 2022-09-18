f038530eacf77abb9da96a42b3ae8b7017857ed4
shengfan <945838085@qq.com>
2022-09-19 02:05:53 +0800

    2022-09-19 shengfan commit 工银澳门面试题

--- src/views/components-demo/count-price.vue
+++ src/views/components-demo/count-price.vue
@@ -1,43 +1,75 @@
 <template>
-  <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px"
-           style="width: 400px; margin-left:50px;"
-  >
-    <el-form-item label="开启活动1">
-      <el-switch
-        v-model="temp.value1"
-        active-value="1"
-        inactive-value="0"
-      />
-      <span style="color: red; font-size: 8px; margin-left: 10px">*草莓限时打8折</span>
-    </el-form-item>
-    <el-form-item label="开启活动2">
-      <el-switch
-        v-model="temp.value2"
-        active-value="1"
-        inactive-value="0"
-      />
-      <span style="color: red; font-size: 8px; margin-left: 10px">*购物满100减10快</span>
-    </el-form-item>
+  <div>
+    <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px"
+             style="width: 400px; margin-left:50px;"
+    >
+      <el-form-item label="开启活动1">
+        <el-switch
+          v-model="temp.value1"
+          active-value="1"
+          inactive-value="0"
+        />
+        <span style="color: red; font-size: 8px; margin-left: 10px">*草莓限时打8折</span>
+      </el-form-item>
+      <el-form-item label="开启活动2">
+        <el-switch
+          v-model="temp.value2"
+          active-value="1"
+          inactive-value="0"
+        />
+        <span style="color: red; font-size: 8px; margin-left: 10px">*购物满100减10快</span>
+      </el-form-item>
+      <el-form-item label="苹果" prop="appleQty">
+        <el-input v-model="temp.appleQty" />
+        <span style="font-size: 8px">8元每斤</span>
+      </el-form-item>
+      <el-form-item label="草莓" prop="strawberryQty">
+        <el-input v-model="temp.strawberryQty" />
+        <span style="font-size: 8px">13元每斤</span>
+      </el-form-item>
+      <el-form-item label="芒果" prop="mangoQty">
+        <el-input v-model="temp.mangoQty" />
+        <span style="font-size: 8px">20元每斤</span>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(temp)">计算总价</el-button>
+      </el-form-item>
+      <el-form-item label="总价" prop="sumPrice">
+        <el-input v-model="temp.sumPrice" :disabled="true" />
+      </el-form-item>
+    </el-form>
 
-    <el-form-item label="苹果" prop="appleQty">
-      <el-input v-model="temp.appleQty"/>
-      <span style="font-size: 8px">8元每斤</span>
-    </el-form-item>
-    <el-form-item label="草莓" prop="strawberryQty">
-      <el-input v-model="temp.strawberryQty"/>
-      <span style="font-size: 8px">13元每斤</span>
-    </el-form-item>
-    <el-form-item label="芒果" prop="mangoQty">
-      <el-input v-model="temp.mangoQty"/>
-      <span style="font-size: 8px">20元每斤</span>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm(temp)">计算总价</el-button>
-    </el-form-item>
-    <el-form-item label="总价" prop="sumPrice">
-      <el-input v-model="temp.sumPrice"/>
-    </el-form-item>
-  </el-form>
+    <el-dialog title="账单" :visible.sync="billDialogVisible" width="60%" center>
+      <div style="text-align: center">
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column type="index" min-width="2%" />
+          <el-table-column label="商品" align="center" min-width="48%">
+            <template slot-scope="scope">
+              <p> {{ scope.row.commodityName }}</p>
+            </template>
+          </el-table-column>
+          <el-table-column label="数量" align="center" min-width="25%">
+            <template slot-scope="scope">
+              <p> {{ scope.row.quality }}</p>
+            </template>
+          </el-table-column>
+          <el-table-column label="价格" align="center" min-width="25%">
+            <template slot-scope="scope">
+              <p> {{ scope.row.itemPrice }}</p>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <span style="display: block; margin-left: 500px; margin-top: 25px; font-size: large">
+        总价:
+        <el-input v-model="temp.sumPrice" :disabled="true" style="border: none; width: 100px" />
+      </span>
+      <span slot="footer" class="dialog-footer">
+<!--        <el-button @click="billDialogVisible = false">取 消</el-button>-->
+        <el-button type="primary" @click="billDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -59,6 +91,8 @@ export default {
         mangoQty: '0',
         sumPrice: '0'
       },
+      tableData: [],
+      billDialogVisible: false,
       rules: {
         appleQty: [{
           required: true,
@@ -103,7 +137,7 @@ export default {
             method: 'post',
             // 后端接口路径
             url: urlStr,
-            // 注意这里使用的是params,该属性负责把属性名和属性值添加到url后面，一般和get配合使用，但也能    和post配合使用
+            // 注意这里使用的是params,该属性负责把属性名和属性值添加到url后面，一般和get配合使用，但也能和post配合使用
             params: {
               // attributeName为属性名，value为属性值
               value1: data.value1,
@@ -113,9 +147,21 @@ export default {
               mangoQty: data.mangoQty
             }
           }).then((response) => {
-            debugger
-            console.log(response)
-            this.temp.sumPrice = response.data.items.totalPrice
+            if (response.status === 200) {
+              this.$message({
+                message: '成功计算!',
+                type: 'success'
+              })
+              this.temp.sumPrice = response.data.items.totalPrice
+              this.billDialogVisible = true
+              debugger
+              this.tableData = response.data.billDetailList
+            } else {
+              this.$message({
+                message: '无数据!',
+                type: 'warning'
+              })
+            }
             // eslint-disable-next-line handle-callback-err
           }).catch((error) => {
             // 对error进行处理
